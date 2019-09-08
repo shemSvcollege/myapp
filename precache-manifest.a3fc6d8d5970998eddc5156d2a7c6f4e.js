97e82118c106b64dac9634c8c4e76a6c3227a0a1
shem bar <shem@svcollege.co.il>
2019-09-08 16:34:58 +0300

    Updates

---
+++ precache-manifest.a3fc6d8d5970998eddc5156d2a7c6f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1d9fce681abdcef2301c6cc1a4dda0",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "928026d633aa825bf393",
+    "url": "/myapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1061127799586579e2d0",
+    "url": "/myapp/static/js/2.034520e3.chunk.js"
+  },
+  {
+    "revision": "928026d633aa825bf393",
+    "url": "/myapp/static/js/main.d5087c51.chunk.js"
+  },
+  {
+    "revision": "67b02876eb6e8b22ce18",
+    "url": "/myapp/static/js/runtime~main.60a23ea0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
